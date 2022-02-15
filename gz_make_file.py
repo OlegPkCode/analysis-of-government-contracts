5f21fdbf95c6fe38afc043be9ce2e42c9ed7ddf9
prol-it <98325731+prol-it@users.noreply.github.com>
2022-02-15 07:39:15 +0300

    Add files via upload

---
+++ gz_make_file.py
@@ -0,0 +1,8 @@
+import os
+ls = [i for i in os.listdir() if i.endswith('.csv')]
+ls.sort()
+with open('all_data.csv','w') as f:
+    for j in ls:
+        s = open(j).read()
+        f.write(s)
+        f.write('\n')
