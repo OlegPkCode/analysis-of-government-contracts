e99b96cfaa8d7e6e3f72d4875b8a4cf9a8bcf7b4
Oleg Prikhodko <4.oleg.pr@gmail.com>
2022-08-01 15:34:05 +0600

    step 5

---
+++ step5_make_dataset.py
@@ -0,0 +1,19 @@
+'''Выгружает спарсенные данные из таблицы positions в csv файл дальнейшей очистки и анализа'''
+
+import sqlite3 as sq
+from lib_gz import *
+import csv
+
+file_output = data_path + 'dataset_positions.csv'
+
+with sq.connect(file_db) as con:
+    cur = con.cursor()
+    sql = 'SELECT DISTINCT name, name_dop, qty, unit, price, total, contract, year, customer, find_text FROM positions'
+
+    with open(file_output, 'w') as file:
+        writer = csv.writer(file, delimiter=';')
+        # Записываем наименования столбцов
+        file.write('name;name_dop;qty;unit;price;total;contract;year;customer;find_text' + '\n')
+        for i in cur.execute(sql).fetchall():
+            writer.writerow(i)
+
