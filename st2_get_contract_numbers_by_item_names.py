6239cd2896ea2a6b31e8a16bbf7ade97130cc010
Oleg Prikhodko <4.oleg.pr@gmail.com>
2023-03-24 23:54:02 +0600

    refactor: change algoritm

--- st2_get_contract_numbers_by_item_names.py
+++ st2_get_contract_numbers_by_item_names.py
@@ -15,19 +15,31 @@
 
 import requests
 from bs4 import BeautifulSoup
+import datetime
 import csv
 import os
 import time
 import sqlite3 as sq
 from lib_gz import data_path, convert_str, file_db, convert_num
 
-start_date = '01.01.2017'
-end_date = '31.12.2022'
+# start_date = '01.01.2017'
+# end_date = '31.12.2022'
 file_input = data_path + 'products.csv'
 test_difference = 0
+# list_date = [
+#     '01.01.2017,31.12.2022',
+# ]
+list_date = [
+    '01.01.2017,31.12.2017',
+    '01.01.2018,31.12.2018',
+    '01.01.2019,31.12.2019',
+    '01.01.2020,31.12.2020',
+    '01.01.2021,31.12.2021',
+    '01.01.2022,31.12.2022',
+]
 
 
-def get_rows(name_pos, num_page):
+def get_rows(name_pos, num_page, start_date, end_date):
     searchString = lambda: '' if len(name_pos) == 0 else 'searchString=' + name_pos + '&'
 
     url1 = 'https://zakupki.gov.ru/epz/contract/search/results.html?' + searchString()
@@ -63,6 +75,7 @@ def get_rows(name_pos, num_page):
             r = requests.get(url, headers=HEADERS)
             soup = BeautifulSoup(r.text, 'html.parser')
             rows = soup.find_all('div', class_='row no-gutters registry-entry__form mr-0')
+            time.sleep(5)
             break
         except Exception as exp:
             # если возникла какая-либо ошибка
@@ -77,59 +90,65 @@ if __name__ == "__main__":
     # Открываем файл file_input, считываем продукты в список
     list_products = []
     with open(file_input, 'r') as file:
-        for i in file:
-            list_products.append(i[:-1])
+        text = file.read()
+
+    for i in text.split('\n'):
+        list_products.append(i)
 
     # Парсим контракты, содержащие данные продукты
     for find_text in list_products:
 
         products = find_text.split(',')
         for product in products:
+            for item_list_date in list_date:
+                start_date, end_date = item_list_date.split(',')
+                print(f"Период с {start_date} по {end_date} ----- ----- ----- ----- -----")
 
-            num_page = 1
-            sum_row = 0
-            list_contracts = []
+                num_page = 1
+                sum_row = 0
+                list_contracts = []
 
-            # Получаем коллекцию BeautifulSoup согласно заданным параметрам
-            rows = get_rows(product, num_page)
+                # Получаем коллекцию BeautifulSoup согласно заданным параметрам
+                rows = get_rows(product, num_page, start_date, end_date)
 
-            while len(rows) > 0:
-                print('Page', num_page, product)
-                for item in rows:
-                    # Номер контракта
-                    contract_num = item.find('a').text.strip()[2:]
-                    # Год исполнения контракта
-                    contract_year_complite = item.find_all('div', class_='data-block__value')[1].text.strip()[-4::]
-                    # Список позиций контракта
-                    contract_list_products = item.find('span', class_='pl-0 col').find('a')
-                    # Заказчик
-                    contract_customer = convert_str(item.find('div', class_='registry-entry__body-href').text.strip())
-                    # Берем общую сумму позиций данного контакта на сайте
-                    total_in_site = convert_num(item.find('div', class_='price-block__value').text.strip())
+                while len(rows) > 0:
+                    print('Page', num_page, product)
+                    for item in rows:
+                        # Номер контракта
+                        contract_num = item.find('a').text.strip()[2:]
+                        # Год исполнения контракта
+                        contract_year_complite = item.find_all('div', class_='data-block__value')[1].text.strip()[-4::]
+                        # Список позиций контракта
+                        contract_list_products = item.find('span', class_='pl-0 col').find('a')
+                        # Заказчик
+                        contract_customer = convert_str(
+                            item.find('div', class_='registry-entry__body-href').text.strip())
+                        # Берем общую сумму позиций данного контакта на сайте
+                        total_in_site = convert_num(item.find('div', class_='price-block__value').text.strip())
 
-                    # Если данный контракт содержит электронную версию, и контракта нет в списке, то сохраняем его
-                    if contract_list_products is not None:
-                        contract_exist = 0
-                        for i in list_contracts:
-                            if contract_num in i:
-                                contract_exist = 1
-                        if contract_exist == 0:
-                            list_contracts.append(
-                                contract_num + ';' + contract_year_complite + ';' + total_in_site + ';' + find_text + ';' + contract_customer)
-                            sum_row += 1
+                        # Если данный контракт содержит электронную версию, и контракта нет в списке, то сохраняем его
+                        if contract_list_products is not None:
+                            contract_exist = 0
+                            for i in list_contracts:
+                                if contract_num in i:
+                                    contract_exist = 1
+                            if contract_exist == 0:
+                                list_contracts.append(
+                                    contract_num + ';' + contract_year_complite + ';' + total_in_site + ';' + find_text + ';' + contract_customer)
+                                sum_row += 1
 
-                # Листаем страницы
-                print('Total row =', sum_row)
-                num_page = num_page + 1
-                rows = get_rows(product, num_page)
-                time.sleep(5)
+                    # Листаем страницы
+                    datetime_now = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+                    print(f"Total row = {sum_row}, Обработка: {datetime_now}")
+                    num_page = num_page + 1
+                    rows = get_rows(product, num_page, start_date, end_date)
 
-            # Записываем результат в файл
-            file_output = data_path + 'list_products_in_contracts_' + product + '_from_' + start_date + '_to_' + end_date + '_rows_' + str(
-                sum_row) + '.csv'
-            with open(file_output, 'a', newline='') as file:
-                writer = csv.writer(file, delimiter='\n')
-                writer.writerow(list_contracts)
+                # Записываем результат в файл
+                file_output = data_path + 'list_products_in_contracts_' + product + '_from_' + start_date + '_to_' + end_date + '_rows_' + str(
+                    sum_row) + '.csv'
+                with open(file_output, 'a', newline='') as file:
+                    writer = csv.writer(file, delimiter='\n')
+                    writer.writerow(list_contracts)
 
     '''
     Заходит в папку <data_path> текущего проекта, берет все файлы *.csv и конкотинирует их в файл all.csv
