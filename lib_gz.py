8124e748b6eb47a0937aff45d7b4963e1369b007
Oleg Prikhodko <4.oleg.pr@gmail.com>
2023-07-19 16:14:58 +0600

    refactor: modified lib.gz

--- lib_gz.py
+++ lib_gz.py
@@ -1,45 +1,34 @@
 import os
 import re
-
 import pandas as pd
 
 data_path = './data/'
 file_db = 'gz.sqlite3'
 
 
-# Удаляем впередистоящие знаки отличные от символьных, дублирующие пробелы, кавычки и знаки ';'
-def convert_str(x):
-    x = x.strip()
-    x = ' '.join(x.split())
-    x = x.replace('"', '')
-    x = x.replace("'", '')
-    x = x.replace(';', '')
-
-    while len(x) > 0 and not x[0].isalpha():
-        x = x[1:]
-
-    return x
+def clean_str(x: str) -> str:
+    """Removes leading non-alphabetic characters, redundant spaces, quotes and semicolons."""
+    x = ' '.join(x.split()).replace('"', '').replace("'", '').replace(';', '')
+    return x.lstrip('0123456789 ')
 
 
-# Удаляем из все символы, кроме чисел, точки и запятой
-def convert_num(x):
-    x = re.sub(r"[^\d.,]", "", x)
-    return x
+def clean_num(x: str) -> str:
+    """Removes all characters except numbers, dot and comma."""
+    return re.sub(r"[^\d.,]", "", x)
 
 
-# Переворачиваем запятую на точку в числе
-def convert_num_dot(x):
+def replace_comma(x: str) -> str:
+    """Replaces comma with dot in a number."""
     return x.replace(',', '.')
 
 
-# Функция конвертирует все файлы ods в каталоге data_path в файлы csv и формирует итоговый файл file_name в формате csv
 def convert_ods_to_csv(file_output):
-    list_files_ods = [i for i in os.listdir(data_path) if i.endswith('.ods')]
-    list_files_ods.sort()
-
+    """Converts all ods files in the data_path directory to csv files and
+    generates a final file file_name in csv format."""
+    list_files_ods = sorted([i for i in os.listdir(data_path) if i.endswith('.ods')])
     with open(file_output, 'w') as f:
         for item in list_files_ods:
-            print('Обработка файла', item)
-            file_csv = data_path + item[:-4] + '.csv'
-            read_file = pd.read_excel(data_path + item)
-            read_file.to_csv(file_csv, index=False, sep=';', header=(lambda *x: list_files_ods.index(item) == 0)())
+            print(f'Processing file {item}')
+            file_csv = f'{data_path}{item[:-4]}.csv'
+            read_file = pd.read_excel(f'{data_path}{item}')
+            read_file.to_csv(file_csv, index=False, sep=';', header=bool(list_files_ods.index(item) == 0))
