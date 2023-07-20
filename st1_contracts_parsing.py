6b08588dde04e40e4b9f0c116c12990276a7b15b
Oleg Prikhodko <4.oleg.pr@gmail.com>
2023-07-20 20:32:43 +0600

    refactor: data parsing moved to get_soup function in lib_gz, changed variable names

--- st1_contracts_parsing.py
+++ st1_contracts_parsing.py
@@ -4,54 +4,37 @@ The format of the file_input file is:
 Contract Number; Year; Customer
 The result is recorded in the file_output file.
 '''
-import requests
-from bs4 import BeautifulSoup
-import time
-import csv
-from lib_gz import data_path, clean_str, clean_num, replace_comma
 
-file_input = data_path + 'contracts.csv'
-file_output = data_path + 'contract_items.csv'
+import csv
+import time
+
+from lib_gz import data_path, clean_str, clean_num, replace_comma, get_soup
+
+file_input = f'{data_path}contracts.csv'
+file_output = f'{data_path}contract_items.csv'
+
 
 def parsing_contract(contract, customer):
-    URL_HEADER = 'https://zakupki.gov.ru/epz/contract/contractCard/payment-info-and-target-of-order.html?reestrNumber=' + contract + '&#contractSubjects'
-    URL_ITEMS = 'https://zakupki.gov.ru/epz/contract/contractCard/payment-info-and-target-of-order-list.html?reestrNumber=' + contract + '&page=1&pageSize=200'
-    HEADERS = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
-    }
-
-    while True:
-        try:
-            # Считываем заголовок контракта и позиции в контракте
-            r_head = requests.get(URL_HEADER, headers=HEADERS)
-            soup_head = BeautifulSoup(r_head.text, 'html.parser')
-            r_pos = requests.get(URL_ITEMS, headers=HEADERS)
-            soup_pos = BeautifulSoup(r_pos.text, 'html.parser')
-            # position_list = soup_pos.find_all('tr', class_='tableBlock__row')
-            position_list = soup_pos.find('tbody', class_='tableBlock__body').find_all('tr', class_='tableBlock__row')
-            time.sleep(5)
-            break
-        except Exception as exp:
-            # если возникла какая-либо ошибка
-            print('Ошибка соединения. Пауза 1 мин.')
-            time.sleep(60)
+    HEADER_URL = 'https://zakupki.gov.ru/epz/contract/contractCard/payment-info-and-target-of-order.html?reestrNumber=' + contract + '&#contractSubjects'
+    ITEMS_URL = 'https://zakupki.gov.ru/epz/contract/contractCard/payment-info-and-target-of-order-list.html?reestrNumber=' + contract + '&page=1&pageSize=200'
+    header_soup = get_soup(HEADER_URL)
+    contract_soup = get_soup(ITEMS_URL)
+    position_list = contract_soup.find('tbody', class_='tableBlock__body').find_all('tr', class_='tableBlock__row')
 
     data = []
     pos = 0
     total = 0
-    total_in_site = 0
 
-    # Заполняем список data позициями контракта
+    # Fill the data list with contract positions
     for item in position_list:
         name = item.find('div', class_='padBtm5 inline js-expand-all-list--not-count')
         qtyUnit = item.find('div', class_='align-items-center')
         priceAndSum = item.find_all('td', class_='tableBlock__col tableBlock__col_right')
-        if (name is not None) and (qtyUnit is not None) and (priceAndSum is not None):
+        if name and qtyUnit and priceAndSum:
             name = clean_str(name.text)
             name_dop = clean_str(item.find_all('td', class_='tableBlock__col')[2].text)
 
-            # Преобразование столбцов количества и единиц измерений
+            # Converting quantity and measurement units columns
             qtyUnit = qtyUnit.text.strip()
             try:
                 qty, unit = qtyUnit.split('\n')
@@ -61,7 +44,7 @@ def parsing_contract(contract, customer):
                 qty = 0
                 unit = clean_str(qtyUnit)
 
-            # Преобразование столбцов цены и суммы
+            # Converting price and sum columns
             price = clean_num(priceAndSum[0].text.strip())
             sum = priceAndSum[1].text.strip()
             sum = clean_num(sum[:sum.find('\n')])
@@ -73,18 +56,18 @@ def parsing_contract(contract, customer):
                 {'name': name, 'name_dop': name_dop, 'qtyUnit': qtyUnit, 'qty': qty, 'unit': unit, 'price': price,
                  'sum': sum})
 
-    # Записываем год исполнения контракта
-    year_finish = soup_head.find('div', class_='date mt-auto').find_all('div', class_='cardMainInfo__section')[
+    # Record the year the contract was fulfilled
+    year_finish = header_soup.find('div', class_='date mt-auto').find_all('div', class_='cardMainInfo__section')[
                       1].text.strip()[-4:]
 
-    # Полное наименование заказчика
-    customer_full_name = soup_head.find('span', class_='cardMainInfo__content').text.strip()
+    # Full name of the customer
+    customer_full_name = header_soup.find('span', class_='cardMainInfo__content').text.strip()
 
-    # Берем общую сумму позиций данного контакта на сайте
-    total_in_site = clean_num(soup_pos.find('td', class_='tableBlock__col tableBlock__col_right cost').text.strip())
+    # Take the total amount of positions of this contract on the site
+    total_in_site = clean_num(header_soup.find('td', class_='tableBlock__col tableBlock__col_right cost').text.strip())
     total_in_site = float(replace_comma(total_in_site))
 
-    # Если общая сумма симпортированых позиций равна сумме контракта на сайте - пишем результат в файл
+    # If the total amount of imported positions equals the contract sum on the site - write the result to the file
     if round(total, 2) == total_in_site:
         with open(file_output, 'a', newline='') as file:
             writer = csv.writer(file, delimiter=';')
@@ -103,27 +86,29 @@ def parsing_contract(contract, customer):
                         customer
                     )
                 )
+
         print(
-            f'Контракт: {contract}, Год: {year_finish}, Позиций - {pos}, Заказчик: {customer}, Обработка: {time.strftime("%H:%M:%S")}')
-        print(f'Сумма импортированных позиций - {total}')
-        print(f'Сумма позиций по данным с сайта - {total_in_site}', end="\n\n")
+            f'Contract: {contract}, Year: {year_finish}, Positions - {pos}, Customer: {customer}, Processing: {time.strftime("%H:%M:%S")}')
+        print(f'Sum of imported positions - {total}')
+        print(f'Sum of positions according to the site data - {total_in_site}', end="\n\n")
     else:
-        print('!!!!! СУММА ИМПОРТИРОВАННЫХ ПОЗИЦИЙ РАСХОДИТСЯ С ОБЩЕЙ СУММОЙ КОНТРАКТОВ НА САЙТЕ!!!!!')
-        print('Позиций - ', pos)
-        print('Сумма импортированных позиций - ', total)
-        print('Сумма позиций на сайте - ', total_in_site)
+        print('!!!!! THE SUM OF IMPORTED POSITIONS DOES NOT MATCH THE TOTAL CONTRACT SUM ON THE SITE!!!!!')
+        print('Positions - ', pos)
+        print('Sum of imported positions - ', total)
+        print('Sum of positions on the site - ', total_in_site)
+
+
+def load_contracts(file_input: str):
+    with open(file_input, 'r') as file:
+        contracts = [line.strip() for line in file]
+    return contracts
 
 
 if __name__ == "__main__":
 
-    # Открываем файл file_input, считываем контракты в список
-    list_contract = []
-    with open(file_input, 'r') as file:
-        for i in file:
-            list_contract.append(i[:-1])
+    contracts = load_contracts(file_input)
 
-    # Парсим контракты из списка
-    for item in list_contract:
+    # Parse contracts from the list
+    for item in contracts:
         contract, year, customer = item.split(';')
         parsing_contract(contract, customer)
-        time.sleep(9)
